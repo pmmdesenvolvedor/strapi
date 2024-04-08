c7946303bb72574ec2b10c2995eeb2692d564f55
Paulo Martinelli <pmmdesenvolvedor@gmail.com>
2024-04-07 23:00:16 -0300

    installed pg, aws-s3 upload provider em updated config files

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,28 @@
-module.exports = () => ({});
+module.exports = () => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        s3Options: {
+          // @ts-ignore
+          accessKeyId: env('AWS_ACCESS_KEY_ID'),
+          // @ts-ignore
+          secretAccessKey: env('AWS_ACCESS_SECRET'),
+          // @ts-ignore
+          region: env('AWS_REGION', 'eu-west-1'),
+          params: {
+            Bucket: env('AWS_BUCKET_NAME'),
+          }
+        }
+      },
+      actionOptions: {
+        upload: {
+          ACL: null
+        },
+        uploadStream: {
+          ACL: null
+        }
+      }
+    }
+  }
+});
